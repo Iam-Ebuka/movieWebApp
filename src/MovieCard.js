e9de51636bc9e9a32f22428971bd76c67a480984
Iam-Ebuka <ebukagrizz@gmail.com>
2023-09-15 14:36:48 +0100

    final touch

--- src/MovieCard.js
+++ src/MovieCard.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
+import { FaHeart, FaRegHeart } from "react-icons/fa";
 import './MovieCard.css'
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
 
 
@@ -14,7 +15,7 @@ const MovieCard = (props) => {
   
 }
 
-  const  API_IMG ="https://image.tmdb.org/t/p/w500/"
+  const  API_IMG ="https://image.tmdb.org/t/p/original/"
 
 
   return (
@@ -22,17 +23,17 @@ const MovieCard = (props) => {
     <p  id='like' onClick={likeBotton}>
       {likeIt ? 
       // Render the filled heart icon when liked
-      <i style={{fontSize: "30px", color: '#be123c', zIndex: '3', fontWeight: 'bolder', cursor: 'pointer' }} class="fa fa-heart" ></i>
+      <FaHeart className="love" style={{fontSize: "30px", color: '#be123c', zIndex: '3', fontWeight: 'bolder', cursor: 'pointer' }} />
        : 
        // Render the outline heart icon when not liked
-       <i style={{fontSize: "30px", color: '#be123c', zIndex: '3', fontWeight: 'bolder', cursor: 'pointer' }} class="fa fa-heart-o" ></i>
+       <FaRegHeart style={{fontSize: "30px", color: '#be123c', zIndex: '3', fontWeight: 'bolder', cursor: 'pointer' }} />
         }
     </p>
     <Link className="linkCard" to={`movies/${props.id}`}>
         <div className='card' data-testid="movie-card" >
         
-            <div className='poster' >
-                <img data-testid='movie-poster' src={props.poster_path ? API_IMG + props.poster_path : "https://plus.unsplash.com/premium_photo-1664302644902-7bf58fa20ef9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1368&q=80" } alt="movies"/>
+            <div className='poster'>
+                <img   data-testid='movie-poster' src={props.poster_path ? API_IMG + props.poster_path : "https://plus.unsplash.com/premium_photo-1664302644902-7bf58fa20ef9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1368&q=80" } alt="movies"/>
             </div>
 
             <div className='info'>
